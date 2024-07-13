7c73a3760a1462b3ad6c17775c267f7c5feb89b0
Giacomo Rossetto <jackyman_cs4@live.it>
2024-07-13 13:14:03 +0000

    Linted with black

--- inventree_gallery_plugin/gallery.py
+++ inventree_gallery_plugin/gallery.py
@@ -1,43 +1,50 @@
 """Sample implementation for ActionMixin."""
 
 from plugin import InvenTreePlugin
-from plugin.mixins import EventMixin, PanelMixin, ActionMixin, APICallMixin, SettingsMixin
+from plugin.mixins import PanelMixin
 from stock.views import StockItemDetail
 from stock.models import StockItem, StockItemAttachment
 from part.views import PartDetail
 from part.models import Part, PartAttachment
 
+
 class GalleryPlugin(PanelMixin, InvenTreePlugin):
     """An EXTREMELY simple action plugin which demonstrates the capability of the ActionMixin class."""
 
-    NAME = 'AttachmentGalleryPlugin'
-    SLUG = 'attachment_gallery'
-    TITLE = 'AttachmentGallery'
+    NAME = "AttachmentGalleryPlugin"
+    SLUG = "attachment_gallery"
+    TITLE = "AttachmentGallery"
 
     def is_image(self, attachment):
-         return True
+        return True
 
     def get_custom_panels(self, view, request):
-            panels = []
+        panels = []
 
-            if isinstance(view, StockItemDetail) or isinstance(view, PartDetail):
+        if isinstance(view, StockItemDetail) or isinstance(view, PartDetail):
 
-                self.item=view.get_object()
-                self.attachments = []
+            self.item = view.get_object()
+            self.attachments = []
 
-                if isinstance(self.item, StockItem):
-                    item_attachments = StockItemAttachment.objects.filter(stock_item = self.item)
+            if isinstance(self.item, StockItem):
+                item_attachments = StockItemAttachment.objects.filter(
+                    stock_item=self.item
+                )
 
-                if isinstance(self.item, Part):
-                    item_attachments = PartAttachment.objects.filter(part = self.item)
+            if isinstance(self.item, Part):
+                item_attachments = PartAttachment.objects.filter(part=self.item)
 
-                self.attachments = list(filter(lambda attachment: self.is_image(attachment), item_attachments))
+            self.attachments = list(
+                filter(lambda attachment: self.is_image(attachment), item_attachments)
+            )
 
-                if len(self.attachments) > 0: 
-                    panels.append({
-                        'title': 'Attachment Gallery',
-                        'icon': 'fa-paperclip',
-                        'content_template': 'gallery/gallery.html',
-                    })
+            if len(self.attachments) > 0:
+                panels.append(
+                    {
+                        "title": "Attachment Gallery",
+                        "icon": "fa-paperclip",
+                        "content_template": "gallery/gallery.html",
+                    }
+                )
 
-            return panels
+        return panels
